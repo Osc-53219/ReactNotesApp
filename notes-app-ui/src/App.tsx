c5a64a6d2bb35fd3ca4f75b511566ce2af69a95e
Osc-53219 <102975104+Osc-53219@users.noreply.github.com>
2023-10-03 15:52:15 -0500

    Fixed form tag and added css to page

--- notes-app-ui/src/App.tsx
+++ notes-app-ui/src/App.tsx
@@ -5,27 +5,30 @@ const App = () => {
     <div className="app-container">
       <form className="note-form">
         <input
-        placeholder="title"
+        placeholder="Title"
         required
         ></input>
+
         <textarea
         placeholder="Content"
         rows={10}
         required
         ></textarea>
-        <button type="submit">
-          Add Note
-        </button>
-        <div className="notes-grid">
-          <div className="note-item">
-            <div className="notes-header">
-              <button>X</button>
-            </div>
-            <h2>Note Title</h2>
-            <p>Note Content</p>
+
+          <button type="submit">
+            Add Note
+          </button>
+        </form>
+
+      <div className="notes-grid">
+        <div className="note-item">
+          <div className="notes-header">
+            <button>x</button>
           </div>
+          <h2>Note Title</h2>
+          <p>Note Content</p>
         </div>
-      </form>
+      </div>
     </div>
   );
 };
